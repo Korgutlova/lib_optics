a59aefd1fcce9f59927c3a7b1f2733206ea746ed
KadyrovAmir <amir.kadyrov.97@mail.ru>
2020-04-06 22:43:00 +0300

    fixed incorrect refraction angle

--- function/main.py
+++ function/main.py
@@ -2,14 +2,14 @@ from function.OpticBuilder import OpticalBuilder
 from function.RefractionLightClass import RefractionLightClass
 
 if __name__ == "__main__":
-    # r = RefractionLightClass()
-    # r.build_graph(42, "water", 1)
+    r = RefractionLightClass()
+    r.build_graph(25, 1, 2.3)
 
-    opt_build = OpticalBuilder()
-    opt_build.focal_length = 2
-    print(opt_build.get_optical_power())
-    print(opt_build)
-
-    opt_build.height_subject = 5
-    opt_build.dist_subject = 3
-    opt_build.execute()
+    # opt_build = OpticalBuilder()
+    # opt_build.focal_length = 2
+    # print(opt_build.get_optical_power())
+    # print(opt_build)
+    #
+    # opt_build.height_subject = 5
+    # opt_build.dist_subject = 3
+    # opt_build.execute()
