5ca8e817f78ab6174a5f820ef53f4fe9e48ce244
korgutlova <korgutlova97@yandex.ru>
2020-04-07 19:17:37 +0300

    add images

--- function/main.py
+++ function/main.py
@@ -2,11 +2,23 @@ from function.OpticBuilder import OpticalBuilder
 from function.RefractionLightClass import RefractionLightClass
 
 if __name__ == "__main__":
-    # r = RefractionLightClass()
-    # r.build_graph(25, 1, 2.3)
 
-    # r = RefractionLightClass()
-    # r.build_graph(30, "water", "air")
+    # преломление света
+    r = RefractionLightClass()
+    r.build_graph(40, "water", "air")
 
+    # точка и рассеивающая линза
     x = OpticalBuilder(dist_subject=10, focal_length=5, height_subject=0, biconcave=True)
+    x.execute()
+
+    # предмет и рассеивающая линза
+    x = OpticalBuilder(dist_subject=10, focal_length=5, height_subject=5, biconcave=True)
+    x.execute()
+
+    # предмет и собирательная линза
+    x = OpticalBuilder(dist_subject=10, focal_length=5, height_subject=5, biconcave=False)
+    x.execute()
+
+    # предмет внутри фокуса собирательной линзы
+    x = OpticalBuilder(dist_subject=4, focal_length=5, height_subject=5, biconcave=False)
     x.execute()
