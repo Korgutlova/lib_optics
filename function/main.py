7c446b3a14a3d2c1bf3cbd1615ab644b4425a554
KadyrovAmir <amir.kadyrov.97@mail.ru>
2020-04-13 20:44:17 +0300

    fixed total internal reflection angles

--- function/main.py
+++ function/main.py
@@ -3,8 +3,12 @@ from function.RefractionLightClass import RefractionLightClass
 
 if __name__ == "__main__":
     # преломление света
-    # r = RefractionLightClass()
-    # r.build_graph(40, "water", "air")
+    r = RefractionLightClass()
+    r.build_graph(40, "water", "air")
+
+    # отражение света
+    r = RefractionLightClass()
+    r.build_graph(28, 3, 1)
 
     # # точка и рассеивающая линза
     # x = OpticalBuilder(dist_subject=10, focal_length=5, height_subject=0, biconcave=True)
@@ -13,11 +17,11 @@ if __name__ == "__main__":
     # # предмет и рассеивающая линза
     # x = OpticalBuilder(dist_subject=15, focal_length=10, height_subject=10, biconcave=True)
     # x.execute()
-
-    # предмет на фокусном расстоянии и собирательная линза
-    x = OpticalBuilder(dist_subject=10, focal_length=10, height_subject=5, biconcave=False)
-    x.execute()
-
+    #
+    # # предмет на фокусном расстоянии и собирательная линза
+    # x = OpticalBuilder(dist_subject=10, focal_length=10, height_subject=5, biconcave=False)
+    # x.execute()
+    #
     # # предмет и собирательная линза
     # x = OpticalBuilder(dist_subject=15, focal_length=8, height_subject=5, biconcave=False)
     # x.execute()
