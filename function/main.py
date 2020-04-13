2af38ee64216f544cdb70c6e6a844ee87f83e3f3
FrostFlame <serazetdinow.damir@yandex.ru>
2020-04-13 22:58:32 +0300

    разбиение линз на классы

--- function/main.py
+++ function/main.py
@@ -1,4 +1,4 @@
-from function.OpticBuilder import OpticalBuilder
+from function.OpticBuilder import Lens, BiconcaveLens, BiconvexLens
 from function.RefractionLightClass import RefractionLightClass
 
 if __name__ == "__main__":
@@ -7,21 +7,21 @@ if __name__ == "__main__":
     # r.build_graph(40, "water", "air")
 
     # точка и рассеивающая линза
-    x = OpticalBuilder(dist_subject=10, focal_length=5, height_subject=0, biconcave=True)
+    x = BiconcaveLens(dist_subject=10, focal_length=5, height_subject=0)
     x.execute()
 
     # предмет и рассеивающая линза
-    x = OpticalBuilder(dist_subject=15, focal_length=10, height_subject=10, biconcave=True)
+    x = BiconcaveLens(dist_subject=15, focal_length=10, height_subject=10)
     x.execute()
 
     # предмет на фокусном расстоянии и собирательная линза
-    x = OpticalBuilder(dist_subject=10, focal_length=10, height_subject=5, biconcave=False)
+    x = BiconvexLens(dist_subject=10, focal_length=10, height_subject=5)
     x.execute()
 
     # предмет на фокусном расстоянии и собирательная линза
-    x = OpticalBuilder(dist_subject=15, focal_length=8, height_subject=5, biconcave=False)
+    x = BiconvexLens(dist_subject=15, focal_length=8, height_subject=5)
     x.execute()
 
     # предмет внутри фокуса собирательной линзы
-    x = OpticalBuilder(dist_subject=4, focal_length=5, height_subject=5, biconcave=False)
+    x = BiconvexLens(dist_subject=4, focal_length=5, height_subject=5)
     x.execute()
