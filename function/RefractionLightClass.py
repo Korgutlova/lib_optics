cd314bffa5a8e0a69c7f032a87bab29b504a5dc4
korgutlova <korgutlova97@yandex.ru>
2020-04-18 15:24:51 +0300

    add check for refraction light class

--- function/RefractionLightClass.py
+++ function/RefractionLightClass.py
@@ -1,4 +1,6 @@
 import math
+import numbers
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -30,34 +32,32 @@ class RefractionLightClass:
     """
 
     def get_angle_refraction(self, angle_incidence: float, medium_one, medium_two) -> float:
-        medium_one, medium_two = self.__validate_index_name(medium_one, medium_two)
-        """
-        
-        sin(a)/sin(y) = n2 / n1
-        
-        http://ru.solverbook.com/spravochnik/zakony-fiziki/zakon-prelomleniya-sveta/
-        
-        """
-        # TODO try catch
-
-        # синус (для вода/воздух 0,67, всё правильно)
+        self.__check_angle(angle_incidence)
+        medium_one = self.__validate_index_name(medium_one)
+        medium_two = self.__validate_index_name(medium_two)
         result_sin = math.sin(math.radians(angle_incidence)) * medium_one / medium_two
         if result_sin > 1:
             result_sin = -math.sin(math.radians(angle_incidence))
-        # дальше надо взять арксинус и перевести в градусы
         return math.degrees(math.asin(result_sin))
 
-    def __validate_index_name(self, medium_one, medium_two):
-        if type(medium_one) == str:
-            medium_one = self.get_refractive_index(medium_one)
-        if type(medium_two) == str:
-            medium_two = self.get_refractive_index(medium_two)
-        return medium_one, medium_two
+    def __validate_index_name(self, medium):
+        type_m = type(medium)
+        if type_m == str:
+            medium = self.get_refractive_index(medium)
+        else:
+            if isinstance(medium, numbers.Number):
+                if medium < 1 or medium > 10:
+                    raise errors.InvalidRefractiveIndex(f'Недопустимый индекс "{medium}" для среды. '
+                                                        f'Допустимый индекс должен входить в рамки [1, 10]')
+            else:
+                raise errors.InvalidRefractiveIndex(f'Недопустимый "{type_m}" тип для индекса среды')
+        return medium
 
     def build_graph(self, angle_incidence: float, first_index, second_index):
         plt.annotate(first_index if type(first_index) == str else "Начальная среда", xy=(-1.5, 0.1))
         plt.annotate(second_index if type(second_index) == str else "Конечная среда", xy=(0.5, 0.1))
-        first_index, second_index = self.__validate_index_name(first_index, second_index)
+        first_index = self.__validate_index_name(first_index)
+        second_index = self.__validate_index_name(second_index)
         second_angle = self.get_angle_refraction(angle_incidence, first_index, second_index)
         plt.annotate(angle_incidence, xy=(-0.6, -0.2))
         plt.annotate(math.floor(abs(second_angle)), xy=(0.5 if second_angle > 0 else -0.6, 0.3))
@@ -149,3 +149,11 @@ class RefractionLightClass:
                         curve_y.append(elem2)
 
         plt.plot(curve_x, curve_y, "black")
+
+    @staticmethod
+    def __check_angle(angle):
+        if not isinstance(angle, numbers.Number):
+            raise errors.InvalidArgumentForAngle(f'Недопустимый "{angle}" тип для угла')
+        if angle <= 0 or angle >= 90:
+            raise errors.InvalidArgumentForAngle(f'Угол "{angle}" некорректен. '
+                                                 f'Допустимый угол должен входить в рамки (1; 90)')
--- util/errors.py
+++ util/errors.py
@@ -1,8 +1,18 @@
-class RefractiveIndexNotFound(Exception):
+class RefractiveIndexNotFound(ValueError):
     def __init__(self, media):
         super(RefractiveIndexNotFound, self).__init__(f'Индекс среды "{media}" не найден.')
+
+
+class InvalidRefractiveIndex(ValueError):
+    def __init__(self, message):
+        super(InvalidRefractiveIndex, self).__init__(message)
 
 
 class InvalidArgumentForLens(ValueError):
     def __init__(self, message):
         super(InvalidArgumentForLens, self).__init__(message)
+
+
+class InvalidArgumentForAngle(ValueError):
+    def __init__(self, message):
+        super(InvalidArgumentForAngle, self).__init__(message)
